802c2e954323c4d70febc2b1aa82c5f65049a841
evgeny <vishniakovevgeniy@gmail.com>
2018-06-23 13:35:36 +0300

    html

--- Sport_shop/index-products.php
+++
@@ -1,4 +0,0 @@
-<?php require 'head.php'; ?>
-<?php require 'header.php'; ?>
-<?php require 'products.php'; ?>
-<?php require 'footer.php'; ?>
--- Sport_shop/index.php
+++
@@ -1,7 +0,0 @@
-
-<?php require 'head.php'; ?>
-<body>  
-<?php require 'header.php'; ?>
-    
-<?php require 'main.php'; ?>
-<?php require 'footer.php'; ?>			       
